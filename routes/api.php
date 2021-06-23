e2b1ff3d32eec3dd1b2c9753e75450714f4bbd60
shahriar201 <shahriarbrine201@gmail.com>
2021-06-23 20:59:56 +0600

    Api JWT token for auth

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,16 @@ use Illuminate\Http\Request;
 Route::apiResource('/class', 'Api\ClassController');
 Route::apiResource('/subject', 'Api\SubjectController');
 Route::apiResource('/student', 'Api\StudentController');
+
+Route::group([
+
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
